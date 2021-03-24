ec22271b4fa299cfec4d49472504600ba6c5ed04
Alfonso PA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2021-03-24 22:02:17 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,39 @@
+import logging
 import os
 import qrcode
 from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, Filters
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ChatAction
-
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ChatAction, KeyboardButton
 
 INPUT_TEXT_STATE = 0
 
+#Configurar logging
+logging.basicConfig(
+    level= logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s,"
+)
+logger = logging.getLogger()
 
 def start_command_handler(update, context):
+    logger.info(f"El usuario {update.effective_user['username']}, ha iniciado una conversacion")
 
     update.message.reply_text(
-        text='¡Hola, patetico humano! \n\n Usa /qr para generar un codigo qr.',
+        text='¡Hola, patetico humano! \n ¿Que necesitas?',
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton(text='Generar QR', callback_data='qr')],
+            [InlineKeyboardButton(text='Comandos', callback_data='comandos')],
             [InlineKeyboardButton(text='Sobre el autor', url='https://twitter.com/AlfonsoA7X')],
         ])
     )
+
+
+def comandos_command_handler(update, context):
+    user_id = update.effective_user['id']
+    logger.info(f"El usuario {user_id}, ha solicitado la lista de comandos")
+    update.message.reply_text(
+        text='Estos son los comandos disponibles.',
+        reply_markup=
+            KeyboardButton(text='UNO', action='UNO')
+    )
+
+
 
 
 def uno_command_handler(update, context):
@@ -28,6 +46,9 @@ def mimir_command_handler(update, context):
     update.message.chat.send_animation(
         animation=open('mimir.mp4', 'rb')
     )
+
+def error(update, context):
+    print(f"Update{update} caused error {context.error}")
 
 
 #Funcion QR
@@ -76,12 +97,26 @@ def send_qr(filename, chat):
 
 #Funcion QR END
 def qr_callback_handler(update, context):
-
     query = update.callback_query
     query.answer()
 
     query.edit_message_text(
         text='Enviame un texto para generarte un codigo QR.'
+    )
+
+    return INPUT_TEXT_STATE
+
+
+def comandos_callback_handler(update, context):
+    query = update.callback_query
+    query.answer()
+
+    query.edit_message_text(
+        text='Estos son los comandos disponibles.',
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text='UNO', action='UNO')],
+            [InlineKeyboardButton(text='mimir', action='mimir')],
+        ])
     )
 
     return INPUT_TEXT_STATE
@@ -96,6 +131,7 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler('start', start_command_handler))
     dp.add_handler(CommandHandler('UNO', uno_command_handler))
     dp.add_handler(CommandHandler('mimir', mimir_command_handler))
+    dp.add_handler(CommandHandler('comandos', comandos_command_handler))
 
     dp.add_handler(ConversationHandler(
         entry_points=[
@@ -110,5 +146,7 @@ if __name__ == '__main__':
         fallbacks=[]
     ))
 
+    dp.add_error_handler(error)
     updater.start_polling()
+    print("BOT_CARGADO")
     updater.idle()
