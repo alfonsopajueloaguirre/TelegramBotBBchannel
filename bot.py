8eca9f50cd1b70634c61292c1bfa603a1e9e61b0
AlfonsoPA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2021-03-24 23:10:02 +0100

    Funcionalidades básicas terminadas

--- bot.py
+++ bot.py
@@ -10,57 +10,48 @@ logging.basicConfig(
 )
 logger = logging.getLogger()
 
+#Bloque de comandos
 def start_command_handler(update, context):
     logger.info(f"El usuario {update.effective_user['username']}, ha iniciado una conversacion")
 
     update.message.reply_text(
-        text='¡Hola, patetico humano! \n ¿Que necesitas?',
+        text='¡Hola, patetico humano!\n¿Necesitas ayuda? Usa /help',
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton(text='Comandos', callback_data='comandos')],
             [InlineKeyboardButton(text='Sobre el autor', url='https://twitter.com/AlfonsoA7X')],
         ])
     )
 
 
-def comandos_command_handler(update, context):
-    user_id = update.effective_user['id']
+def help_command_handler(update, context):
+    user_id = update.effective_user['username']
     logger.info(f"El usuario {user_id}, ha solicitado la lista de comandos")
     update.message.reply_text(
-        text='Estos son los comandos disponibles.',
-        reply_markup=
-            KeyboardButton(text='UNO', action='UNO')
+        text='Estos son los comandos disponibles.\n\n/UNO - como que UNO?\n/mimir - a la cama',
     )
 
 
 def uno_command_handler(update, context):
+    user_id = update.effective_user['username']
+    logger.info(f"El usuario {user_id}, ha solicitado la imagen UNO")
     update.message.chat.send_photo(
         photo=open('UNO.jpeg', 'rb')
     )
 
 
 def mimir_command_handler(update, context):
+    user_id = update.effective_user['username']
+    logger.info(f"El usuario {user_id}, ha solicitado el gif de saber")
     update.message.chat.send_animation(
         animation=open('mimir.mp4', 'rb')
     )
 
+
+#Configuracion de error
 def error(update, context):
     print(f"Update{update} caused error {context.error}")
 
-def comandos_callback_handler(update, context):
-    query = update.callback_query
-    query.answer()
 
-    query.edit_message_text(
-        text='Estos son los comandos disponibles.',
-        reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton(text='UNO', action='UNO')],
-            [InlineKeyboardButton(text='mimir', action='mimir')],
-        ])
-    )
-
-    return INPUT_TEXT_STATE
-
-
+#Bloque de main
 if __name__ == '__main__':
 
     updater = Updater(token='1791382674:AAFZ9l1jxH-wHXpFeapw5zDJ4ZelTB9XEV8', use_context=True)
@@ -70,7 +61,7 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler('start', start_command_handler))
     dp.add_handler(CommandHandler('UNO', uno_command_handler))
     dp.add_handler(CommandHandler('mimir', mimir_command_handler))
-    dp.add_handler(CommandHandler('comandos', comandos_command_handler))
+    dp.add_handler(CommandHandler('help', help_command_handler))
 
     dp.add_error_handler(error)
 
