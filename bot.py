88155e0e37e961614fe1da89318ee8dda0567084
Alfonso PA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2021-03-25 14:58:26 +0100

    Added several commands
    /lasemilla
    /amigos
    Text 27

--- bot.py
+++ bot.py
@@ -1,18 +1,19 @@
 import logging
 import os
 from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, Filters
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ChatAction, KeyboardButton
-
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ChatAction, KeyboardButton, Bot
 
 #Configurar logging
+from telegram.files import sticker
+
 logging.basicConfig(
-    level= logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s,"
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s."
 )
 logger = logging.getLogger()
 
 #Bloque de comandos
 def start_command_handler(update, context):
-    logger.info(f"El usuario {update.effective_user['username']}, ha iniciado una conversacion")
+    logger.info(f"El usuario {update.effective_user['username']}, ha saludado al bot")
 
     update.message.reply_text(
         text='¡Hola, patetico humano!\n¿Necesitas ayuda? Usa /help',
@@ -26,7 +27,8 @@ def help_command_handler(update, context):
     user_id = update.effective_user['username']
     logger.info(f"El usuario {user_id}, ha solicitado la lista de comandos")
     update.message.reply_text(
-        text='Estos son los comandos disponibles.\n\n/UNO - como que UNO?\n/mimir - a la cama',
+        text='Estos son los comandos disponibles.\n\n/UNO - como que UNO?\n/mimir - a la cama\n'
+             '/lasemilla - invoca a Jose\n/amigos - ver codigos de amigos'
     )
 
 
@@ -40,21 +42,54 @@ def uno_command_handler(update, context):
 
 def mimir_command_handler(update, context):
     user_id = update.effective_user['username']
-    logger.info(f"El usuario {user_id}, ha solicitado el gif de saber")
+    logger.info(f"El usuario {user_id}, ha solicitado el gif de Saber")
     update.message.chat.send_animation(
         animation=open('mimir.mp4', 'rb')
     )
 
 
+def semilla_command_handler(update, context):
+    user_id = update.effective_user['username']
+    logger.info(f"El usuario {user_id}, ha solicitado la imagen de la semilla")
+    update.message.chat.send_photo(
+        photo=open('semilla.jpg', 'rb')
+    )
+
+
+def amigos_command_handler(update, context):
+    user_id = update.effective_user['username']
+    logger.info(f"El usuario {user_id}, ha solicitado la lista de amigos")
+    update.message.reply_text(
+        text='Códigos de amigo\nNA\nCarlos: 020349652\nSócrates: 250095682\nLoreneas: 647824428\nKibou: 859216994\n'
+             'Pochaco: 673107507\nLopeta: 740711472\nHawk: 622192147\nSerjunpe: 762791833\nPendrat: 088828445\n'
+             'Jaggles: 005172928\nVicky: 244515342\nSpaiky: 849736116\n\nJP\nAlf: 042474419\nKatsumi: 464519697'
+    )
+
+
+def veintisiete(update, context):
+    if update.message.text.find("27") > 0:
+        user_id = update.effective_user['username']
+        logger.info(f"El usuario {user_id}, ha invocado el número magico")
+        update.message.chat.send_sticker(
+            sticker='CAACAgQAAxkBAAMmYFyUL2a8ZnTwl3GgNBvHoNOHEKoAAqgBAAIghU0O5euI3ERF0YceBA'
+        )
+
+
+def sticker_id(update, context):
+    print(update.message.sticker)
+
+
 #Configuracion de error
 def error(update, context):
-    print(f"Update{update} caused error {context.error}")
+    print(f"La update: {update} ha causado el error: {context.error}")
 
 
-#Bloque de main
+#Bloque de main token original:1791382674:AAFZ9l1jxH-wHXpFeapw5zDJ4ZelTB9XEV8 token prueba:1788135825:AAGK8O5i7bPxyFyOoKyC-rAyGHyMFc8Q3Hk
 if __name__ == '__main__':
 
-    updater = Updater(token='1791382674:AAFZ9l1jxH-wHXpFeapw5zDJ4ZelTB9XEV8', use_context=True)
+#Bot_BB = Bot("1788135825:AAGK8O5i7bPxyFyOoKyC-rAyGHyMFc8Q3Hk")
+
+    updater = Updater(token='1788135825:AAGK8O5i7bPxyFyOoKyC-rAyGHyMFc8Q3Hk', use_context=True)
 
     dp = updater.dispatcher
 
@@ -62,10 +97,14 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler('UNO', uno_command_handler))
     dp.add_handler(CommandHandler('mimir', mimir_command_handler))
     dp.add_handler(CommandHandler('help', help_command_handler))
+    dp.add_handler(CommandHandler('lasemilla', semilla_command_handler))
+    dp.add_handler(CommandHandler('amigos', amigos_command_handler))
+    dp.add_handler(MessageHandler(Filters.text, veintisiete))
+    dp.add_handler(MessageHandler(Filters.sticker, sticker_id))
 
     dp.add_error_handler(error)
 
     updater.start_polling()
-    print("BOT_CARGADO")
+    print("BOT_LISTO")
 
     updater.idle()
