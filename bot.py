8d9effa1b2ff13c0787afd09081f62832e5ef698
Alfonso PA <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2021-03-31 00:56:06 +0200

    Added two new commands, edited one former command

--- bot.py
+++ bot.py
@@ -28,7 +28,8 @@ def help_command_handler(update, context):
     logger.info(f"El usuario {user_id}, ha solicitado la lista de comandos")
     update.message.reply_text(
         text='Estos son los comandos disponibles:\n\n/UNO - ¿Cómo que UNO?\n/mimir - a la cama\n'
-             '/lasemilla - invoca a Jose\n/27 - el numero mágico\n/amigos - ver códigos de amigos'
+             '/lasemilla - invoca a Jose\n/27 - el numero mágico\n/amigos - ver códigos de amigos\n/teparto - revienta un otaku culiao\n'
+             '/arcueid - la mejor'
     )
 
 
@@ -42,9 +43,9 @@ def uno_command_handler(update, context):
 
 def mimir_command_handler(update, context):
     user_id = update.effective_user['username']
-    logger.info(f"El usuario {user_id}, ha solicitado el gif de Saber")
-    update.message.chat.send_animation(
-        animation=open('mimir.mp4', 'rb')
+    logger.info(f"El usuario {user_id}, ha solicitado mimir con Saber")
+    update.message.chat.send_photo(
+        photo=open('mimir.png', 'rb')
     )
 
 
@@ -53,6 +54,14 @@ def semilla_command_handler(update, context):
     logger.info(f"El usuario {user_id}, ha solicitado la imagen de la semilla")
     update.message.chat.send_photo(
         photo=open('semilla.jpg', 'rb')
+    )
+
+
+def te_parto_command_handler(update, context):
+    user_id = update.effective_user['username']
+    logger.info(f"El usuario {user_id}, ha solicitado la imagen parte otakus")
+    update.message.chat.send_photo(
+        photo=open('teparto.jpg', 'rb')
     )
 
 
@@ -71,8 +80,14 @@ def veintisiete(update, context):
         user_id = update.effective_user['username']
         logger.info(f"El usuario {user_id}, ha invocado el número magico")
         update.message.chat.send_sticker(
-            sticker='CAACAgQAAxkBAAMmYFyUL2a8ZnTwl3GgNBvHoNOHEKoAAqgBAAIghU0O5euI3ERF0YceBA'
+            sticker='CAACAgQAAxkBAAM7YGOrTmm_xGNJA0hhCXH5-ilsE0oAAqgBAAIghU0O5euI3ERF0YceBA'
         )
+    if update.message.text.find("Arcueid") > 0:
+        user_id = update.effective_user['username']
+        logger.info(f"El usuario {user_id}, ha nombrado a la entidad suprema")
+        update.message.chat.send_photo(
+            photo=open('arcueid.jpg', 'rb')
+        )    
 
 
 def sticker_id(update, context):
@@ -89,7 +104,7 @@ if __name__ == '__main__':
 
 #Bot_BB = Bot("1788135825:AAGK8O5i7bPxyFyOoKyC-rAyGHyMFc8Q3Hk")
 
-    updater = Updater(token='1791382674:AAFZ9l1jxH-wHXpFeapw5zDJ4ZelTB9XEV8', use_context=True)
+    updater = Updater(token='1788135825:AAGK8O5i7bPxyFyOoKyC-rAyGHyMFc8Q3Hk', use_context=True)
 
     dp = updater.dispatcher
 
@@ -99,6 +114,7 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler('help', help_command_handler))
     dp.add_handler(CommandHandler('lasemilla', semilla_command_handler))
     dp.add_handler(CommandHandler('amigos', amigos_command_handler))
+    dp.add_handler(CommandHandler('teparto', te_parto_command_handler))
     dp.add_handler(MessageHandler(Filters.text, veintisiete))
     dp.add_handler(MessageHandler(Filters.sticker, sticker_id))
 
